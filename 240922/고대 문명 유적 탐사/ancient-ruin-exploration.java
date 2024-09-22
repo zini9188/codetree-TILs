a8df38c81259a11545bb4aa293c9eed3b59a5b44
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-09-22 19:05:57 +0900

    Title: 고대 문명 유적 탐사, Time: 132ms, Memory: 11MB, Status: Passed - CodeTree

--- 240922/고대 문명 유적 탐사/ancient-ruin-exploration.java
+++ 240922/고대 문명 유적 탐사/ancient-ruin-exploration.java
@@ -3,158 +3,56 @@ import java.util.*;
 
 public class Main {
 
-    private static int[][] originPieces;
     private static Queue<Integer> newValues;
     private static int K, M;
-
     private static int[] dx = new int[]{-1, -1, -1, 0, 1, 1, 1, 0},
             dy = new int[]{-1, 0, 1, 1, 1, 0, -1, -1};
     private static int[] dx2 = {-1, 1, 0, 0}, dy2 = {0, 0, 1, -1};
+    private static Square origin;
 
     public static void main(String[] args) throws IOException {
-        // 유물
-        // 1. 격자 회전, 90 180 270
-        // 2. 가장 큰 회전 방법 찾기
-        // 3. 찾은 회전 방법으로 회전하고 유물 획득
-        // 4. 유물을 획득하면 빈 공간에 유물을 채워넣음
-        // 5. 채워넣었을때 유물 획득 가능하면 다시 4.
-        // 6. K번 반복
-
         input();
         solution();
     }
 
     private static void solution() {
         // K번 반복
-        for (int i = 0; i < K; i++) {
+        for (int repeat = 0; repeat < K; repeat++) {
             // 회전 격자를 찾음.
             int maxScore = 0;
-            int[] selectInfo = selectPieces();
-            // 가장 점수가 큰 회전 정보를 바탕으로 실제 배열 회전
-            if (selectInfo == null) {
-                break;
-            }
 
-            rotate(selectInfo[0], selectInfo[1], selectInfo[2], originPieces);
+            Square maxSquare = null;
+            for (int k = 1; k <= 3; k++) {
+                for (int j = 1; j <= 3; j++) {
+                    for (int i = 1; i <= 3; i++) {
+                        Square rotate = origin.rotate(i, j, k);
+                        int score = rotate.calcScore();
 
-            // 회전한 배열에서 유물 삭제
-            while (true) {
-                fill();
-                int score = detectPiece(originPieces);
-                if (score == 0) {
-                    break;
-                }
-                maxScore += score;
-            }
-
-            System.out.print(maxScore + " ");
-        }
-    }
-
-    private static void fill() {
-        // 삭제된 곳에 유물 채워넣음
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 5; j > 0; j--) {
-                if (originPieces[j][i] == 0) {
-                    originPieces[j][i] = newValues.poll();
-                }
-            }
-        }
-    }
-
-    private static int detectPiece(int[][] arr) {
-        int score = 0;
-        boolean[][] visited = new boolean[6][6];
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
-                if (visited[i][j]) {
-                    continue;
-                }
-
-                Queue<int[]> q = new ArrayDeque<>();
-                Queue<int[]> d = new ArrayDeque<>();
-                q.add(new int[]{i, j});
-                d.add(new int[]{i, j});
-                visited[i][j] = true;
-                int cnt = 1;
-                while (!q.isEmpty()) {
-                    int[] cur = q.poll();
-
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur[0] + dx2[dir];
-                        int ny = cur[1] + dy2[dir];
-
-                        if (nx >= 1 && nx <= 5 && ny >= 1 && ny <= 5 && !visited[nx][ny]
-                                && arr[nx][ny] == arr[i][j]) {
-                            visited[nx][ny] = true;
-                            q.add(new int[]{nx, ny});
-                            d.add(new int[]{nx, ny});
-                            cnt++;
+                        if (score > maxScore) {
+                            maxScore = score;
+                            maxSquare = rotate;
                         }
                     }
                 }
-
-                if (cnt >= 3) {
-                    score += d.size();
-                    while (!d.isEmpty()) {
-                        int[] p = d.poll();
-                        arr[p[0]][p[1]] = 0;
-                    }
-                }
             }
-        }
-        return score;
-    }
 
-    private static int[][] copy(int[][] origin) {
-        int[][] temp = new int[6][6];
-        // 배열 복사
-        for (int i = 0; i < 6; i++) {
-            System.arraycopy(origin[i], 0, temp[i], 0, 6);
-        }
-        return temp;
-    }
-
-    private static int[] selectPieces() {
-        // 좌표, 회전 정보
-        int[] info = new int[3];
-        int maxCnt = 0;
-        // 격자는 행이 작고, 열이 작아야 함.
-        for (int k = 1; k <= 3; k++) {
-            for (int j = 2; j <= 4; j++) {
-                for (int i = 2; i <= 4; i++) {
-                    // 회전 배열 가져옴
-                    int[][] temp = copy(originPieces);
-                    rotate(i, j, k, temp);
-
-                    // 획득 가능한 유물 개수 파악
-                    int cnt = detectPiece(temp);
-
-                    if (cnt > maxCnt) {
-                        info[0] = i;
-                        info[1] = j;
-                        info[2] = k;
-                        maxCnt = cnt;
-                    }
-                }
+            if (maxSquare == null) {
+                break;
             }
-        }
 
-        if (maxCnt == 0) {
-            return null;
-        }
+            origin = maxSquare;
 
-        return info;
-    }
+            // 회전한 배열에서 유물 삭제
+            while (true) {
+                origin.fill();
+                int res = origin.calcScore();
+                if (res == 0) {
+                    break;
+                }
+                maxScore += res;
+            }
 
-
-    // 회전한 복사 배열
-    private static void rotate(int x, int y, int rotate, int[][] arr) {
-        int[][] copy = copy(originPieces);
-        int nDir = 2 * rotate;
-        for (int i = 0; i < 8; i++) {
-            arr[x + dx[nDir]][y + dy[nDir]] = copy[x + dx[i]][y + dy[i]];
-            nDir = (nDir + 1) % 8;
+            System.out.print(maxScore + " ");
         }
     }
 
@@ -167,11 +65,11 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         // 초기 배열 세팅
-        originPieces = new int[6][6];
-        for (int i = 1; i <= 5; i++) {
+        origin = new Square();
+        for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= 5; j++) {
-                originPieces[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 5; j++) {
+                origin.arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
@@ -180,6 +78,89 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             newValues.add(Integer.valueOf(st.nextToken()));
+        }
+    }
+
+    public static class Square {
+
+        int[][] arr;
+
+        public Square() {
+            arr = new int[5][5];
+        }
+
+        public Square rotate(int x, int y, int rotate) {
+            Square square = new Square();
+
+            for (int i = 0; i < 5; i++) {
+                System.arraycopy(arr[i], 0, square.arr[i], 0, 5);
+            }
+
+            int nDir = 2 * rotate;
+            for (int i = 0; i < 8; i++) {
+                square.arr[x + dx[nDir]][y + dy[nDir]] = arr[x + dx[i]][y + dy[i]];
+                nDir = (nDir + 1) % 8;
+            }
+
+            return square;
+        }
+
+        public void fill() {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 4; j >= 0; j--) {
+                    if (arr[j][i] == 0) {
+                        arr[j][i] = newValues.poll();
+                    }
+                }
+            }
+        }
+
+        public int calcScore() {
+            int score = 0;
+            boolean[][] visited = new boolean[5][5];
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (visited[i][j]) {
+                        continue;
+                    }
+
+                    Queue<int[]> q = new ArrayDeque<>();
+                    Queue<int[]> d = new ArrayDeque<>();
+                    q.add(new int[]{i, j});
+                    d.add(new int[]{i, j});
+                    visited[i][j] = true;
+                    int cnt = 1;
+                    while (!q.isEmpty()) {
+                        int[] cur = q.poll();
+
+                        for (int dir = 0; dir < 4; dir++) {
+                            int nx = cur[0] + dx2[dir];
+                            int ny = cur[1] + dy2[dir];
+
+                            if (inRange(nx, ny) && !visited[nx][ny]
+                                    && arr[nx][ny] == arr[i][j]) {
+                                visited[nx][ny] = true;
+                                q.add(new int[]{nx, ny});
+                                d.add(new int[]{nx, ny});
+                                cnt++;
+                            }
+                        }
+                    }
+
+                    if (cnt >= 3) {
+                        score += d.size();
+                        while (!d.isEmpty()) {
+                            int[] p = d.poll();
+                            arr[p[0]][p[1]] = 0;
+                        }
+                    }
+                }
+            }
+            return score;
+        }
+
+        private boolean inRange(int x, int y) {
+            return x >= 0 && x < 5 && y >= 0 && y < 5;
         }
     }
 }
